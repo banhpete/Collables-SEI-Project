7760da1139b4cd605e8ca973815dc9ccb8377f56
Pebanh <peter.banh95@gmail.com>
2020-07-31 07:49:58 -0400

    Add additional code/file for Heroku deployment

--- server.js
+++ server.js
@@ -13,15 +13,22 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
 
 // API Routes
 app.use('/api/user', require('./routes/api/users'));
 app.use('/api/table', require('./routes/api/table'));
 
 // Serving React App
-
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // Server Start
-app.listen(3001, function () {
-  console.log("Hi, I'm running on 3000")
+const port = process.env.PORT || 3001;
+
+app.listen(port, function () {
+  console.log(`Express app running on port ${port}`)
 })
